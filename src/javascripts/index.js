cfb49aacbb4686fa11862b79fabb0dc3e92f4521
Nathan Birrell <nathanbirrell@gmail.com>
2018-02-09 11:05:30 +1100

    Replace client get with UserService
    It seems the previous way of fetching user data/locale isn't working well in Chat.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -3,15 +3,21 @@ This is the first JavaScript file that runs once your iframe is loaded within a 
 */
 import ZAFClient from 'zendesk_app_framework_sdk';
 import I18n from 'i18n';
+import UserService from './services/UserService';
 
 // Create a new ZAFClient
 export const client = ZAFClient.init();
 
 // add an event listener to detect once your app is registered with the framework
 client.on('app.registered', (appData) => {
-  client.get('currentUser.locale').then(userData => {
+  console.log('appData', appData);
+
+
+
+  UserService.getCurrentUser().then(userData => {
+    console.log(userData);
     // load translations based on the account's current locale
-    I18n.loadTranslations(userData['currentUser.locale']);
+    I18n.loadTranslations(userData.locale);
     // look up app module for the current location
     let location = appData.context.location;
     let App = require(`./${location}.js`).default;
